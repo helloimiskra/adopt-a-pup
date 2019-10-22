5eb616812b48f5d2a84919a7b69066d8d5206938
github username <github email address>
2019-10-22 13:26:28 +0900

    adds basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :pets
+  resources :shelters
+  resources :sessions
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/signout' => 'sessions#destroy'
+
+  root 'users#welcome'
 end
