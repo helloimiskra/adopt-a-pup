dc6f2e37fa16771ffe040f26749f7d56bf0b1b7a
github username <github email address>
2019-10-23 14:47:26 +0900

    adds helper methods & model methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(name: params[:user][:username])
+        @user = User.find_by(username: params[:user][:username])
 
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
 
     def destroy
         session.delete :user_id
-        redirect_to '/home'
+        redirect_to '/'
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
     validates :username, uniqueness: true
     validates :first_name, format: { without: /[0-9]/, message: "does not allow numbers" }
     validates :last_name, format: { without: /[0-9]/, message: "does not allow numbers" }
+
+    def name
+        self.first_name + " " + self.last_name
+    end
+
 end
