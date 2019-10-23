20f253c59f956b86a3b32329c2a13ae804789867
github username <github email address>
2019-10-23 12:00:52 +0900

    adds crud actions to user and pet model

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,39 @@
 class PetsController < ApplicationController
 
+    def new
+        @pet = Pet.new
+    end
+
+    def create
+        pet = Pet.create(pet_params)
+        if pet.save
+            @pet = pet
+            redirect_to pet_path(@pet)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @pet = Pet.find_by(params[:id])
+    end
+
+    def edit
+    end
+
+
+    def update
+        @pet = Pet.find(params[:id])
+        @pet.update(pet_params)
+        redirect_to pet_path(@pet)
+    end
+
+
+    private
+    
+    def pet_params
+        params.require(:pet).permit(:name, :type, :breed, :description, :adopted)
+    end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,33 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+
+        if @user.save
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+
+    def show
+        @user = User.find_by(id: params[:id])
+        if current_user != @user 
+            redirect_to '/'
+        end
+      
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :password, :email, :shelter_admin)
+    end
 
 end
