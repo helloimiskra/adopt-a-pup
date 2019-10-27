da4ecd14fca32ab373e4de6cb702f54d329d7e7b
github username <github email address>
2019-10-27 22:57:29 +0900

    properly adds nested route

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,10 @@
 
 shelter_admin = User.create(first_name: 'Jane', last_name: 'Smith', username: 'janesmith', password: 'password', email: 'janesmith@janesmith.com', shelter_admin: true)
 avg_user = User.create(first_name: 'Joe', last_name: 'Smith', username: 'joesmith', password: 'password', email: 'joesmith@joesmith.com', shelter_admin: false)
+shelter_admin_two = User.create(first_name: 'Hyori', last_name: 'Lee', username: 'leehyori', password: 'password', email: 'hyori@hyori.com', shelter_admin: true)
 city = City.create(name: 'Seoul')
 shelter_one = Shelter.create(name: "Needs Love", address: "Gangnam Station Exit 2", email: 'needslove@lovepets.com', user_id: 1, city_id: 1 )
+shelter_two = Shelter.create(name: "Karma", address: 'Yongin', email: 'karma@karma.com', user_id: 3, city_id: 1)
 pet_one = Pet.create(name: 'Mishka', pet_type: 'Dog', breed: 'Pembroke Welsh Corgi', description: 'Found on the side of the road. Very friendly to other dogs and humans. Needs a loving home.', shelter_id: 1, user_id: 1, adopted: false)
+pet_two = Pet.create(name: 'Lovely', pet_type: 'Dog', breed: 'Pembroke Welsh Corgi', description: 'Surrendered by previous owner due to poor health. Big cuddle-bug.', shelter_id: 1, user_id: 1, adopted: false)
+pet_three = Pet.create(name: 'Coco', pet_type: 'Cat', breed: 'Persian', description: 'Found crying for mom. Needs foster or adopter immediately.', shelter_id: 2, user_id: 3, adopted: false)
