087b3437d3481ee26d68185fe991d8a932606d1e
github username <github email address>
2019-10-30 23:02:27 +0900

    adjust sessions login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,28 +6,13 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if !params[:user][:username].blank?
-            @user = User.find_by(username: params[:user][:username])
-            if @user && @user.authenticate(params[:user][:password])
-                session[:user_id] = @user.id
-                redirect_to user_path(@user)
-            else
-                redirect_to '/login'
-            end
-        else
-            @user = User.find_or_create_by(uid: auth['uid']) do |u|
-                u.name = auth['info']['name']
-                u.email = auth['info']['email']
-            end
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-            render 'users/welcome'
-            end
-
+            redirect_to user_path(@user)
+        else
+            redirect_to '/login'
         end
-
-            
-       
-
     end
  
     def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
     
     has_many :shelters
   
-
+  
 
 end
